2e545d3b5bd5b12b7bea290a447d27079e834734
staffblack <staffblack@gmail.com>
2023-05-15 11:51:45 -0400

    Este es mi mensaje de commit

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# Copyright © 2018 Garazd Creation (https://garazd.biz)
+# @author: Yurii Razumovskyi (support@garazd.biz)
+# @author: Iryna Razumovska (support@garazd.biz)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
+
+{
+    'name': 'Odoo Facebook Pixel',
+    'version': '16.0.1.0.0',
+    'category': 'Website',
+    'author': 'Garazd Creation',
+    'website': 'https://garazd.biz/shop',
+    'license': 'LGPL-3',
+    'summary': 'Add the Facebook Pixel event PageView to all website pages | Meta Pixel Integration',
+    'images': ['static/description/banner.png', 'static/description/icon.png'],
+    'live_test_url': 'https://garazd.biz/r/KFc',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/res_config_settings_views.xml',
+        'views/website_templates.xml',
+    ],
+    'support': 'support@garazd.biz',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
---
+++ models/website.py
@@ -0,0 +1,12 @@
+# Copyright © 2018 Garazd Creation (<https://garazd.biz>)
+# @author: Yurii Razumovskyi (<support@garazd.biz>)
+# @author: Iryna Razumovska (<support@garazd.biz>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
+
+from odoo import fields, models
+
+
+class Website(models.Model):
+    _inherit = 'website'
+
+    facebook_pixel_key = fields.Char('Meta Pixel ID')
